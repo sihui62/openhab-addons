83923e515c6a3c9436c914b3ce074df125cd2ebe
Gaël L'hopital <gael@lhopital.org>
2019-05-23 12:33:02 +0200

    Creating Glh Extensions Module Adding Thermostat

---
+++ bundles/org.openhab.binding.glh/src/main/java/org/openhab/binding/glh/internal/GlhBindingConstants.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.glh.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link ThermostatBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Gaël L'hopital - Initial contribution
+ */
+@NonNullByDefault
+public class GlhBindingConstants {
+
+    public static final String BINDING_ID = "glh";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_THERMOSTAT = new ThingTypeUID(BINDING_ID, "thermostat");
+
+    // Thermostat Channel ids
+    public static final String CHANNEL_TEMPERATURE = "Temperature";
+    public static final String CHANNEL_HUMIDITY = "Humidity";
+    public static final String CHANNEL_HUMIDEX = "Humidex";
+
+    public static final String CHANNEL_SETPOINT_MODE = "SetpointMode";
+    public static final String CHANNEL_SETPOINT_TEMP = "Sp_Temperature";
+    public static final String CHANNEL_THERM_RELAY = "ThermRelayCmd";
+    public static final String CHANNEL_DURATION = "duration";
+
+    public static final String SETPOINT_MODE_MANUAL = "manual";
+    public static final String SETPOINT_MODE_HG = "hg";
+    public static final String SETPOINT_MODE_OFF = "off";
+    public static final String SETPOINT_MODE_MAX = "max";
+}
