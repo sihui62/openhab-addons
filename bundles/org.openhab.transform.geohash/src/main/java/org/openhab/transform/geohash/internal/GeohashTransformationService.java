3cf52c17995a7d3459906da7bf7d752a433c8ae8
gael@lhopital.org <gael@lhopital.org>
2024-04-19 15:40:21 +0200

    Rebased to OH 4.2
    Signed-off-by: gael@lhopital.org <gael@lhopital.org>

---
+++ bundles/org.openhab.transform.geohash/src/main/java/org/openhab/transform/geohash/internal/GeohashTransformationService.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.transform.geohash.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.PointType;
+import org.openhab.core.transform.TransformationException;
+import org.openhab.core.transform.TransformationService;
+import org.osgi.service.component.annotations.Component;
+
+import ch.hsr.geohash.GeoHash;
+import ch.hsr.geohash.WGS84Point;
+
+/**
+ *
+ * The implementation of {@link GeohashTransformationService} which simply maps coordinates
+ * (latitude,longitude) to corresponding Geohash
+ *
+ * @author Gaël L'hopital - Initial contribution and API
+ */
+@Component(immediate = true, service = TransformationService.class, property = { "smarthome.transform=GEOHASH" })
+@NonNullByDefault
+public class GeohashTransformationService implements TransformationService {
+    private int DEFAULT_PRECISION = 6;
+
+    @Override
+    public @Nullable String transform(final String precision, final String coordinates) throws TransformationException {
+        try {
+            PointType point = new PointType(coordinates);
+            try {
+                int numcar = precision.isEmpty() ? DEFAULT_PRECISION : Integer.parseInt(precision);
+                if (numcar > 0 && numcar <= 12) {
+                    return GeoHash.withCharacterPrecision(point.getLatitude().doubleValue(),
+                            point.getLongitude().doubleValue(), numcar).toBase32();
+                } else {
+                    throw new TransformationException(
+                            String.format("Valid range for Precision is ]0,12] : '{}'", precision));
+                }
+            } catch (NumberFormatException e) {
+                throw new TransformationException(
+                        String.format("The value '{}' is not valid precision level : {}", precision));
+            }
+        } catch (IllegalArgumentException e) {
+            try {
+                GeoHash hash = GeoHash.fromGeohashString(coordinates);
+                WGS84Point centerPoint = hash.getBoundingBox().getCenter();
+                PointType point = new PointType(new DecimalType(centerPoint.getLatitude()),
+                        new DecimalType(centerPoint.getLongitude()));
+                return point.toString();
+            } catch (NullPointerException e2) {
+                throw new TransformationException(String
+                        .format("The value '{}' is not valid geohash nor a valid coordinate expression", coordinates));
+            }
+        }
+    }
+}
---
+++ bundles/org.openhab.transform.geohash/src/main/java/org/openhab/transform/geohash/internal/profiles/GeohashTransformationProfile.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.transform.geohash.internal.profiles;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.library.types.StringType;
+import org.openhab.core.thing.profiles.ProfileCallback;
+import org.openhab.core.thing.profiles.ProfileContext;
+import org.openhab.core.thing.profiles.ProfileTypeUID;
+import org.openhab.core.thing.profiles.StateProfile;
+import org.openhab.core.transform.TransformationException;
+import org.openhab.core.transform.TransformationHelper;
+import org.openhab.core.transform.TransformationService;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+import org.openhab.core.types.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Profile to offer the GeohashTransformationservice on a ItemChannelLink
+ *
+ * @author Gaël L'hopital - initial contribution
+ *
+ */
+@NonNullByDefault
+public class GeohashTransformationProfile implements StateProfile {
+
+    public static final ProfileTypeUID PROFILE_TYPE_UID = new ProfileTypeUID(
+            TransformationService.TRANSFORM_PROFILE_SCOPE, "GEOHASH");
+    private static final String PRECISION_PARAM = "precision";
+    private static final String SOURCE_FORMAT_PARAM = "sourceFormat";
+
+    private final Logger logger = LoggerFactory.getLogger(GeohashTransformationProfile.class);
+    private final TransformationService service;
+    private final ProfileCallback callback;
+
+    private final @NonNullByDefault({}) String precision;
+    private final @NonNullByDefault({}) String sourceFormat;
+
+    public GeohashTransformationProfile(ProfileCallback callback, ProfileContext context,
+            TransformationService service) {
+        this.service = service;
+        this.callback = callback;
+
+        Object paramPrecision = context.getConfiguration().get(PRECISION_PARAM);
+        Object paramSource = context.getConfiguration().get(SOURCE_FORMAT_PARAM);
+
+        logger.debug("Profile configured with '{}'='{}', '{}'={}", PRECISION_PARAM, paramPrecision, SOURCE_FORMAT_PARAM,
+                paramSource);
+        // SOURCE_FORMAT_PARAM is an advanced parameter and we assume "%s" if it is not set
+        if (paramSource == null) {
+            paramSource = "%s";
+        }
+        // PRECISION_PARAM is an advanced parameter and we assume "6" if it is not set
+        if (paramPrecision == null) {
+            paramPrecision = "6";
+        }
+        if (paramPrecision instanceof String && paramSource instanceof String) {
+            precision = (String) paramPrecision;
+            sourceFormat = (String) paramSource;
+        } else {
+            logger.warn("Parameter '{}' and '{}' have to be Strings. Profile will be inactive.", PRECISION_PARAM,
+                    SOURCE_FORMAT_PARAM);
+            precision = null;
+            sourceFormat = null;
+        }
+    }
+
+    @Override
+    public ProfileTypeUID getProfileTypeUID() {
+        return PROFILE_TYPE_UID;
+    }
+
+    @Override
+    public void onStateUpdateFromItem(State state) {
+        if (state instanceof Command command) {
+            callback.handleCommand(command);
+        } else {
+            logger.debug("The given state {} could not be transformed to a command", state);
+        }
+    }
+
+    @Override
+    public void onCommandFromItem(Command command) {
+        callback.handleCommand(command);
+    }
+
+    @Override
+    public void onCommandFromHandler(Command command) {
+        if (precision == null || sourceFormat == null) {
+            logger.warn(
+                    "Please specify a precision and a source format for this Profile in the '{}', and '{}' parameters. Returning the original command now.",
+                    PRECISION_PARAM, SOURCE_FORMAT_PARAM);
+            callback.sendCommand(command);
+            return;
+        }
+        callback.sendCommand((Command) transformState(command));
+    }
+
+    @Override
+    public void onStateUpdateFromHandler(State state) {
+        if (precision == null || sourceFormat == null) {
+            logger.warn(
+                    "Please specify a precision and a source format for this Profile in the '{}', and '{}' parameters. Returning the original state now.",
+                    PRECISION_PARAM, SOURCE_FORMAT_PARAM);
+            callback.sendUpdate(state);
+            return;
+        }
+        callback.sendUpdate((State) transformState(state));
+    }
+
+    private Type transformState(Type state) {
+        String result = state.toFullString();
+        try {
+            result = TransformationHelper.transform(service, precision, sourceFormat, state.toFullString());
+        } catch (TransformationException e) {
+            logger.warn("Could not geohash state '{}' with precision '{}' and format '{}'", state, precision,
+                    sourceFormat);
+        }
+        StringType resultType = new StringType(result);
+        logger.debug("Transformed '{}' into '{}'", state, resultType);
+        return resultType;
+    }
+}
